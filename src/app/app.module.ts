975256cdef8e2cdfbc3344ac5996553742381f81
Eternal_Mangekyo <himanshu.tomar@goplaybook.com>
2018-11-27 09:45:26 +0530

    Remaining module Header and Sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FeedCardComponent } from './components/feed-card/feed-card.component';
 import { HttpClientModule }    from '@angular/common/http';
 import { CreatedAtPipe } from './pipes/created-at.pipe';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FeedCardComponent,
-    CreatedAtPipe
+    CreatedAtPipe,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
